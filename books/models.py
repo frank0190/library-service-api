28986127d8aeb25a1aa314729274dbb7ccee9a6a
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-10 17:52:38 -0500

    Create Book model and register in admin panel

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices.choices
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
+
+    class Meta:
+        ordering = ["name", "author"]
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.author}"
