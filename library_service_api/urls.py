de2249b4f0a9c08fc86b1acaa3b41ffcd990868d
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-11 22:32:12 -0500

    Initialize users app

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
+    path("api/user/", include("users.urls", namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
