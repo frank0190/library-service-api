22778047d34055131729d8eeedc6dcac4a603b00
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-12 23:07:13 -0500

    Add BorrowingListSerializer and BorrowingDetailSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from books.serializers import BookSerializer
 from borrowings.models import Borrowing
 
 
@@ -18,3 +19,19 @@ class BorrowingSerializer(serializers.ModelSerializer):
         if attrs["book"].inventory == 0:
             raise ValidationError("This book is out of stock.")
         return attrs
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
+
+
+class BorrowingDetailSerializer(BorrowingListSerializer):
+    book = BookSerializer()
