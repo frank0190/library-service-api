ce3cc0fb2e788f018ad6aa172c40f871004cd77b
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-13 12:39:08 -0500

    Implement a return Borrowing functionality

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets
+from django.utils.timezone import now
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -40,3 +43,22 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         book.inventory -= 1
         book.save()
         serializer.save(user=self.request.user)
+
+
+    @action(methods=["GET"], detail=True, url_path="return")
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+        if borrowing.is_active:
+            borrowing.is_active = False
+            borrowing.actual_return_date = now().date()
+            borrowing.book.inventory += 1
+            borrowing.book.save()
+            borrowing.save()
+            return Response(
+                {"detail": "The book returned successfully."},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {"detail": "This book is already returned."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
