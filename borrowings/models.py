f129b5ac0eec9a0fa34752d2875045f4410565eb
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-12 22:51:51 -0500

    Create Borrowing model and register in admin panel

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.timezone import now
+from django.conf import settings
+from django.db.models import Q, F
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(default=now)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(expected_return_date__gte=F("borrow_date")),
+                name="expected_return_after_borrow"
+            ),
+            models.CheckConstraint(
+                check=Q(actual_return_date__gte=F("borrow_date")),
+                name="actual_return_after_borrow"
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.book.name} ({self.borrow_date} - {self.expected_return_date})"
