145b64e239d6c2cf1e50451ba5cad11156349bdf
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-12 22:47:24 -0500

    Initialize borrowings app

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "books",
     "users",
+    "borrowings",
 ]
 
 AUTH_USER_MODEL = "users.User"
--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
+    path("api/", include("borrowings.urls", namespace="borrowings")),
     path("api/user/", include("users.urls", namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
