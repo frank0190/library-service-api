6e82afff8afc3df170df8b1d6fa57d8465b9d46a
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-13 13:31:18 -0500

    Add tests for authenticated users

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,7 +9,7 @@ from django.urls import reverse
 
 from borrowings.models import Borrowing
 from books.tests import create_book
-
+from borrowings.serializers import BorrowingListSerializer, BorrowingDetailSerializer
 
 BORROWING_URL = reverse("borrowings:borrowing-list")
 
@@ -20,14 +20,18 @@ def detail_url(borrowing_id: int) -> Response:
 
 def create_borrowing(as_dict: bool=False, **params):
     defaults = {
-        "expected_return_date": datetime(2025, 6, 30),
+        "expected_return_date": date(2025, 6, 30),
         "book": create_book(),
-        "user": get_user_model().objects.create_user(
+        "user": params.get("user") or get_user_model().objects.create_user(
             email="test@user.com", password="test123user"
         )
     }
     defaults.update(params)
-    return defaults if as_dict else Borrowing.objects.create(**defaults)
+    if as_dict:
+        defaults["book"] = defaults["book"].id
+        del defaults["user"]
+        return defaults
+    return Borrowing.objects.create(**defaults)
 
 
 class UnauthenticatedUserTest(TestCase):
@@ -47,3 +51,46 @@ class UnauthenticatedUserTest(TestCase):
 
         for response in responses:
             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedUserTest(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+        self.user_1 = get_user_model().objects.create_user(
+            email="test_1@user.com", password="test123user"
+        )
+        self.user_2 = get_user_model().objects.create_user(
+            email="test_2@user.com", password="test123user"
+        )
+
+        self.borrowing_1 = create_borrowing(user=self.user_1)
+        self.borrowing_2 = create_borrowing(user=self.user_2)
+
+        self.client.force_authenticate(self.user_1)
+
+    def test_borrowing_list(self) -> None:
+        response = self.client.get(BORROWING_URL)
+        borrowings = Borrowing.objects.filter(user_id=self.user_1.id)
+        serializer = BorrowingListSerializer(borrowings, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"], serializer.data)
+
+    def test_borrowing_detail(self) -> None:
+        response = self.client.get(detail_url(self.borrowing_1.id))
+        borrowing = Borrowing.objects.get(id=self.borrowing_1.id)
+        serializer = BorrowingDetailSerializer(borrowing)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_borrowing_create(self) -> None:
+        payload = create_borrowing(as_dict=True)
+        response = self.client.post(BORROWING_URL, payload)
+        borrowing = Borrowing.objects.get(id=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(payload["expected_return_date"], borrowing.expected_return_date)
+        self.assertEqual(payload["book"], borrowing.book.id)
+        self.assertEqual(self.user_1.id, borrowing.user.id)
