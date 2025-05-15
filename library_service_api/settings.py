414e00ac22f41005de7b90e851bf37cb5cccde3a
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-14 23:11:02 -0500

    Configure SECRET_KEY, DEBUG, and ALLOWED_HOSTS via environment variables

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-09n$oq(78m!$i+33hsw%l2g=9t6g8+ay%b@pwz85fay!yi9ir&"
+SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] if DEBUG else ["127.0.0.1", "localhost"]
 
 
 # Application definition
