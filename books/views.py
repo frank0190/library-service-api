33cb1f4f59436d5cf83551ab45e8f2dd34a2beae
Vadym Piddubnyi <leegrad@ukr.net>
2025-05-15 21:50:19 -0500

    Add documentation to books app

--- books/views.py
+++ books/views.py
@@ -1,9 +1,18 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 from books.models import Book
 from books.serializers import BookSerializer
 from books.permissions import IsAdminUserOrReadOnly
 
 
+@extend_schema_view(
+    create=extend_schema(summary="Create book"),
+    list=extend_schema(summary="List books"),
+    retrieve=extend_schema(summary="Get book details"),
+    update=extend_schema(summary="Update book"),
+    partial_update=extend_schema(summary="Partially update book"),
+    destroy=extend_schema(summary="Delete book"),
+)
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
